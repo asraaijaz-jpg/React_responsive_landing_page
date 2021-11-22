2bf30b6829d6a6767c5fdeed6c6d0359b7b48731
asra <asraaijaz.se17@iba-suk.edu.pk>
2021-11-22 17:44:56 +0500

    commit

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -12,41 +12,45 @@ function Navbar()
 {
   return(
     <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <a class="navbar-brand" href="#">Navbar</a>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" style={{marginLeft:'110px'}}><img src={logo2}/></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbarSupportedContent" 
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-   </button>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent"style={{marginLeft:'1006px'}}>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#" style={{marginLeft:'-230px'}}><b>The Reformer</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style={{color:'white' , marginLeft:'-20px'}}><b>Workouts</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#" style={{color:'white' ,  marginLeft:'28px', borderBottom:'6px solid #D9B2FF'}}><b>
+       About</b></a>
+        </li>
+     
+     <li>
+       <img src={f2} style={{width:'30px' , height:'30px'  , marginLeft:'50px'}}/>
+     </li>
 
-   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-         <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-         </li>
-         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-               Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-               <a class="dropdown-item" href="#">Action</a>
-               <a class="dropdown-item" href="#">Another action</a>
-               <div class="dropdown-divider"></div>
-               <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-         </li>
-         <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-         </li>
+     <li>
+       <img src={f1} style={{width:'30px' , height:'30px ' , marginLeft:'30px' , marginLeft:'35px'}}/>
+     </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <form class="d-flex">
+        <button class="btn btn-primary" type="submit" style={{width:'150px' , height:'50px',
+      backgroundColor:'#245AE5' , color:'white'}}><b>SHOP NOW</b></button>
       </form>
-   </div>
+    </div>
+  </div>
 </nav>
+
+  </>
+
 
 </div>
 )
