40e5bf001a0aac142791e401dca2e58516f4e8b0
asra <asraaijaz.se17@iba-suk.edu.pk>
2021-11-22 17:52:23 +0500

    commit

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -15,7 +15,7 @@ function Navbar()
      <>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" style={{marginLeft:'110px'}}><img src={logo2}/></a>
+    <a class="navbar-brand" href="#" style={{marginLeft:'110px'}} id='lside'><img src={logo2} /></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbarSupportedContent" 
     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
